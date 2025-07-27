45235a34e9d90b8cd8167047319aebd31573fc35
Carlosmelo1295 <carlosmelo1295@gmail.com>
2025-07-27 14:56:25 -0300

    algoritmo calculo de raiz quadrada

---
+++ src/poo/Raiz.java
@@ -0,0 +1,55 @@
+package poo;
+
+import java.util.ArrayList;
+
+public class Raiz {
+	ArrayList<Integer> arrayDesc = new ArrayList<>();
+	ArrayList<Integer> arrayAsc = new ArrayList<>();
+	ArrayList<Integer> resultado = new ArrayList<>();
+
+	ArrayList<Integer> calcular(int valor) {
+
+		for (int i = valor; i >= 0; i--) {
+
+			if (i % 2 != 0) {
+				arrayDesc.add(i);
+			}
+
+		}
+
+		for (int j = 0; j <= valor; j++) {
+
+			if (j % 2 != 0) {
+				arrayAsc.add(j);
+
+			}
+
+		}
+
+		int fistRes = arrayDesc.getFirst() - arrayAsc.getFirst();
+		resultado.add(fistRes);
+		System.out.println(arrayDesc.getFirst() + " - " + arrayAsc.getFirst() + " = " + fistRes);
+
+		for (int k = 1; k < valor; k++) {
+
+			arrayDesc.removeFirst();
+			arrayAsc.removeFirst();
+			if (resultado.size() >= 1) {
+				int somaArr = resultado.getFirst() - arrayAsc.getFirst();
+				System.out.println(resultado.getFirst() + " - " + arrayAsc.getFirst() + " = " + somaArr);
+				resultado.remove(resultado.getFirst());
+				resultado.add(somaArr);
+
+				if (somaArr < arrayAsc.getFirst()) {
+					System.out.println("A raiz quadrada de " + valor + " é " + (k+1));
+
+					break;
+				}
+			}
+
+		}
+
+		return arrayDesc;
+	}
+
+}
---
+++ src/poo/RaizTest.java
@@ -0,0 +1,13 @@
+package poo;
+
+//import java.util.Arrays;
+
+public class RaizTest {
+
+	public static void main(String[] args) {
+		Raiz raiz = new Raiz();
+		
+		raiz.calcular(1296);
+
+	}
+}
