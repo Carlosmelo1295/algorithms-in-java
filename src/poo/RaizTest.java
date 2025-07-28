861a34f632d74438bc3077123056e972ab5a7a47
Carlosmelo1295 <carlosmelo1295@gmail.com>
2025-07-28 11:23:22 -0300

    reducao de loops

--- src/poo/RaizTest.java
+++ src/poo/RaizTest.java
@@ -7,7 +7,7 @@ public class RaizTest {
 	public static void main(String[] args) {
 		Raiz raiz = new Raiz();
 		
-		raiz.calcular(64, 1); // 1 para números Pares 0 para ímpares.
+		raiz.calcular(27, 0); // 1 para números Pares, 0 para ímpares.
 
 	}
 }
